6b0156438a48cf2c9950eb2a4ecc4972a44b7d61
Wei Xuan <yeoweixuan6@gmail.com>
2026-06-05 11:58:47 +0000

    week2 hw done

--- week2-HW/gallery.js
+++ week2-HW/gallery.js
@@ -1,23 +1,19 @@
-/*Name this external file gallery.js*/
+function upDate(previewPic) {
 
-function upDate(previewPic){
- /* In this function you should 
-    1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image
-    
-    2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image 
-    */
-  
-	}
+    console.log("Mouse over triggered");
+    console.log(previewPic.alt);
+    console.log(previewPic.src);
 
-	function unDo(){
-     /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    
-    2) Change the text  of the div with the id = "image" 
-    back to the original text.  You can use the html code to see what that original text was
-    */
-		
-	}
+    let imageDiv = document.getElementById("image");
+
+    imageDiv.innerHTML = previewPic.alt;
+    imageDiv.style.backgroundImage = `url('${previewPic.src}')`;
+}
+
+function unDo() {
+
+    let imageDiv = document.getElementById("image");
+
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+}
